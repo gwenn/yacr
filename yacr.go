f4cbe67d992c6094f08c6d034c221b0ff6a353e6
gwenn <gtreguier@gmail.com>
2011-06-02 19:19:01 +0200

    Add a CSV writer.

--- yacr.go
+++ yacr.go
@@ -19,14 +19,14 @@ func DefaultReader(rd io.Reader) *Reader {
 	return NewReader(rd, ',', true)
 }
 func NewReader(rd io.Reader, sep byte, quotes bool) *Reader {
+	// TODO
+	if quotes {
+		panic("Quoted mode not supported yet")
+	}
 	return &Reader{sep: sep, quotes: quotes, b: bufio.NewReader(rd), values: make([][]byte, 20)}
 }
 
 func (r *Reader) ReadRow() ([][]byte, os.Error) {
-	// TODO
-	if r.quotes {
-		panic("Quoted mode not supported yet")
-	}
 	line, err := r.readLine()
 	if err != nil {
 		return nil, err
@@ -69,4 +69,47 @@ func (r *Reader) split(line []byte) [][]byte {
 	a = append(a, line[start:])
 	r.values = a // if cap(a) != cap(r.values)
 	return a
+}
+
+type Writer struct {
+	sep    byte
+	quotes bool
+	//trim	bool
+	b      *bufio.Writer
+}
+
+func DefaultWriter(wr io.Writer) *Writer {
+	return NewWriter(wr, ',', true)
+}
+func NewWriter(wr io.Writer, sep byte, quotes bool) *Writer {
+	// TODO
+	if quotes {
+		panic("Quoted mode not supported yet")
+	}
+	return &Writer{sep: sep, quotes: quotes, b: bufio.NewWriter(wr)}
+}
+
+func (w *Writer) WriteRow(row [][]byte) (err os.Error) {
+	for i, v := range row {
+		if i > 0 {
+			err = w.b.WriteByte(w.sep)
+			if err != nil {
+				return
+			}
+		}
+		err = w.write(v)
+		if err != nil {
+			return
+		}
+	}
+	err = w.b.WriteByte('\n')
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (w *Writer) write(value []byte) (err os.Error) {
+	_, err = w.b.Write(value)
+	return
 }
